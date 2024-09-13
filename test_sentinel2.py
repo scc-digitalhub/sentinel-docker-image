f36a826aae52791a65243b4ad33d90c47ca261b6
Lattaruolo Andrea <lattaruolo.96@gmail.com>
2024-09-13 15:04:59 +0200

    small fix

--- test_sentinel2.py
+++ test_sentinel2.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     download_products(query_df,DOWNLOAD_PATH,download_parameters.user,download_parameters.password,download_parameters.tmp_path_same_folder_dwl)
     preprocess_path = create_path(DOWNLOAD_PATH,PREPROCESS_PATH)
     snap_commands = start_executions(download_parameters=download_parameters,products_dir=DOWNLOAD_PATH,output_dir=preprocess_path)
-    snap_commands = snap_commands.map(lambda x:x.replace('\\\\','\\'))
+    #snap_commandk_s = list(map(lambda x:x.replace('\\\\','\\'),snap_commands))
     print(f"snap commmands sentinel2 {snap_commands}")
     #bandstack_snap_cmd(input_fpath, bands, output_dir, snap_graph_path)
 # sudo docker build -t test-python .
